21e9995f7c5d73602644c9cf071cf5d9581b5027
Nikola Shahpazov <nshahpazov@gmail.com>
2014-11-27 15:02:24 +0200

    added the usage of a model nwo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,17 @@
-define(['backbone'], function (Backbone) {
+define(function (require) {
   'use strict';
+  var $ = require('jquery');
+  var _ = require('underscore');
+  var Backbone = require('backbone');
+  var GameView = require('views/game');
+  var GameModel = require('models/game');
+
   var App = Backbone.View.extend({
 
     el: $('#content'),
 
     initialize: function () { 
     'use strict';
-      console.log('Everything works out fine in th end');
     },
 
     events: {
@@ -19,7 +24,10 @@ define(['backbone'], function (Backbone) {
 
     onClickStartGame: function () {
       'use strict';
-      console.log('game is started');
+      var game = new GameModel(),
+          gameView = new GameView({model: game});
+
+      this.$el.html(gameView.render().$el);
     }
   });
   return App;
