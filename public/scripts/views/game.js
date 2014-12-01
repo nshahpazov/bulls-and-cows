6611e8b945776f0c24c570a1377aa1cef9e55149
Nikola Shahpazov <nshahpazov@gmail.com>
2014-12-01 02:26:39 +0200

    changed, and added more event handling

--- public/scripts/views/game.js
+++ public/scripts/views/game.js
@@ -1,68 +1,82 @@
 define(function (require) {
   'use strict';
-  
+
   var $ = require('jquery'),
-      underscore = require('underscore'),
-      countup = require('utils/countup'),
-      Backbone = require('backbone'),
-      Handlebars = require('handlebars'),
-      sourceTpl = require('text!templates/game.html'),
-      template = Handlebars.compile(sourceTpl);
+    countup = require('utils/countup'),
+    Backbone = require('backbone'),
+    Handlebars = require('handlebars'),
+    sourceTpl = require('text!templates/game.html'),
+    template = Handlebars.compile(sourceTpl),
+    wsocket = require('socket-connection');
 
   var GameView = Backbone.View.extend({
 
     tagName: 'div',
     className: "col-md-8 col-md-offset-4",
-    
+
     id: 'game',
 
     template: template,
 
     events: {
-      'click #ready-btn': 'onClickReadyButton'        
+      'click #ready-button': 'onClickReadyButton',
+      'keyup #numberfield': 'onKeyup'
+    },
+
+    onKeyup: function (event) {
+      var value = event.target.value;
+      if (event.keyCode == 13) {
+        this.ws.send(JSON.stringify({
+          status: 'try',
+          number: value.toString()
+        }));
+      }
     },
 
     onClickReadyButton: function (e) {
       'use strict';
-      this.ws.send(JSON.stringify({ready: true}));
-      // make the button inactive
+      this.ws.send(JSON.stringify({
+        status: 'playerready'
+      }));
     },
 
     startGame: function () {
       'use strict';
-     var timerDOM = $('#timer');
-
-     // definitely optimize this shit later!!!
-     timerDOM.countup(function (d, h, m, s) {
-       timerDOM.html(m + ':' + s);
-     });
-    },
-    
-    // refactor!
-    initialize: function () {
-      'use strict';
-      this.ws = new WebSocket('ws://localhost:3000');
-      var that = this;
-
-      this.ws.onmessage = function (e) {
-        var res = JSON.parse(e.data);
-        
-        // create some controller for that
-        if (res.status === 'gameready') {
-          that.trigger('gameready');
-        } else {
-          if (res.status === 'gamefinished') {
-            that.trigger('gamefinished');
-          }
-        }
-      };
-      
-      this.listenTo(this, 'gameready', function () {
-        // it can be bound
-        that.startGame();
+      var timerDOM = $('#timer');
+      timerDOM.countup(function (d, h, m, s) {
+        timerDOM.html(m + ':' + s);
       });
     },
-  
+
+    initialize: function () {
+      'use strict';
+      wsocket.connect('ws://localhost:3000', this);
+      var username = prompt('Enter your name');
+
+      wsocket.authenticate(username);
+      this.ws = wsocket.ws;
+
+      this.listenTo(this, 'gameready', this.onGameReady);
+      this.listenTo(this, 'gamefinished', this.onGameFinished);
+      this.listenTo(this, 'allplayers', this.onAllPlayers);
+      this.listenTo(this, 'gameresult', this.onGameResult);
+    },
+
+    onGameResult: function (data) {
+      console.log(data.result);
+    },
+
+    onGameReady: function () {
+      'use strict';
+      $('#numberfield').prop('disabled', false);
+      this.startGame();
+    },
+
+    onAllPlayers: function (data) {
+      this.model.set('players', data.players);
+      this.render();
+    },
+
     render: function () {
       'use strict';
       var templ = this.template(this.model.toJSON());
