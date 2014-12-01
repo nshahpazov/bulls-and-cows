795e0a4ad574f748f6be32f05a4d45e696dca0ec
Nikola Shahpazov <nshahpazov@gmail.com>
2014-12-01 18:11:41 +0200

    ws message handling on the clientside

---
+++ public/scripts/ws-controller.js
@@ -0,0 +1,15 @@
+module.exports = {
+  
+  events: {
+    
+  },
+
+  init: function (ws) {
+    'use strict';
+    this.ws = ws;
+    Object.keys(events).forEach(function (key) {
+      
+    });
+  }
+};
+
