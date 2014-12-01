8a861da759e5154f5b6ac8318cc0604f71c485bd
Nikola Shahpazov <nshahpazov@gmail.com>
2014-12-01 18:10:27 +0200

    controller constructor class for partical web socket

---
+++ ws-controller.js
@@ -0,0 +1,78 @@
+var BullsAndCows = require('./bulls-and-cows');
+var _ = require('lodash');
+
+function WSController(wss, ws, control) {
+  'use strict';
+  this.wss = wss;
+  this.ws = ws;
+}
+
+WSController.prototype.initialize = function (control) {  
+  'use strict';
+  var that = this;
+  this.ws.on('message', function (response) {
+    var data = JSON.parse(response);
+    Object.keys(control.events).forEach(function (ev) {
+      if (ev === data.status) {
+        control.handlers[control.events[ev]](data);
+      }
+    });
+  });
+};
+
+module.exports = WSController;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
